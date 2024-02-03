76b49a7f8d5efe45a09b1bf4d0f55ffb03df545a
Angéline <157594445+angelinepi@users.noreply.github.com>
2024-02-03 15:17:57 +0100

    Reapply "Ajout d'un module token et affichage du fond de carte"
    This reverts commit 9c0c79001d8393df7c748ea987813432fc95d8f1.

---
+++ js/token.js
@@ -0,0 +1,7 @@
+// Token MapTiler pour un appel dans plan.js
+//var mapToken = 'zXodaO9ZW510ceX2WoPL';
+//export var mapToken ;
+
+//export {mapToken} ;
+
+export var mapToken = 'zXodaO9ZW510ceX2WoPL';
